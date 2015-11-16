a2fcf5274a520c9d0e4a44ad7e863d20641acac2
Damir Mustafin <frenchthebread@gmail.com>
2015-11-16 20:58:23 +0900

    package.js

--- package.js
+++ package.js
@@ -1,24 +1,16 @@
 Package.describe({
-  name: 'frenchbread:swagger',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'frenchbread:swagger',
+    version: '0.0.10',
+    // Brief, one-line summary of the package.
+    summary: 'Javascript library to connect to swagger-enabled APIs via browser.',
+    // URL to the Git repository containing the source code for this package.
+    git: 'https://github.com/frenchbread/meteor-swagger.git',
+    // By default, Meteor will default to using README.md for documentation.
+    // To avoid submitting documentation, set this field to null.
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('swagger.js');
-});
-
-Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('frenchbread:swagger');
-  api.addFiles('swagger-tests.js');
+    api.versionsFrom('1.2.1');
+    api.addFiles('client/lib/swagger-js/index.js', 'client');
 });
